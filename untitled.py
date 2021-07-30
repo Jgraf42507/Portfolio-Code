8cbd35226a4ff34ca8610b0cef9b91bd7d10535d
Jgraf42507 <60794130+Jgraf42507@users.noreply.github.com>
2021-07-30 12:57:05 -0500

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,6 @@
+def Hello():
+    # This function is for when the assistant 
+    # is called it will say hello and then 
+    # take query
+    speak("hello sir I am your desktop assistant. /
+          Tell me how may I help you")
